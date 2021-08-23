4f9df2532c5c4e3116ede3c95d10b3c80400099a
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-08-23 07:46:00 +0200

    add some boolean tests

---
+++ tests/booleans.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    #[allow(unused_variables)]
+    fn test_declare_constant_boolean_var() {
+        let b: bool;
+    }
+
+    #[test]
+    fn test_declare_prefix_underscore_to_ignore_dead_var() {
+        let _b: bool;
+    }
+}
