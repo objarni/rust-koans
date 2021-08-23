491021818e17577b68e436a55c52424babd7c53f
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-08-23 20:15:05 +0200

    More bool tests

--- tests/booleans.rs
+++ tests/booleans.rs
@@ -11,4 +11,19 @@ mod tests {
     fn test_declare_prefix_underscore_to_ignore_dead_var() {
         let _b: bool;
     }
+
+    #[test]
+    fn test_true() {
+        assert!(true)
+    }
+
+    #[test]
+    fn test_false() {
+        assert!(!false)
+    }
+
+    #[test]
+    fn parse_true() {
+        assert!("true".parse().unwrap())
+    }
 }
